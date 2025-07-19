dfa72a4d1aac7124f54c8f41b17fafbb50828150
somya1527 <somyak2700@gmail.com>
2025-07-19 12:17:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>VRS Calculator</h1>
+      <p>This is a sample VRS calculator base project.</p>
+    </div>
+  )
+}
+
+export default App
